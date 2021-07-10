44bc064ebfeed4dff719b83dd6c841b057837216
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-10 14:45:45 +0000

    [Add] レビュー編集＆削除機能追加(レイアウト仮)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,7 +11,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.user_id = current_user.id
     if @review.save
-      redirect_to root_path
+      redirect_to request.referer
     else
       # renderだとパラメータが消えてしまうので、バリデーションメッセージをフラッシュに入れてredirectし、viewで表示させる
       flash[:review_alert] = @review.errors.full_messages.to_s.gsub!(/"/, '').gsub(/\]/, '').gsub(/\[/, '')
@@ -19,7 +19,20 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    redirect_to user_path(current_user), notice: "レビューを更新しました"
+  end
+
+  def destroy
+    @review = Review.find_by(id: params[:id])
+    @review.destroy
+    redirect_to root_path, alert: "レビューを削除しました"
+  end
+
   def edit
+      @review = Review.find_by(id: params[:id])
   end
 
   private
