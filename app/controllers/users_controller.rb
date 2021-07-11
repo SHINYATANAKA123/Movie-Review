b209638547eaa81ae4f27d1156fd9ce8447575fe
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-11 12:17:39 +0000

    [Add] ユーザー詳細の追加(仮)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
+     @user = User.find_by(id: params[:id])
+      @reviews = @user.reviews.page(params[:page]).reverse_order
   end
 
   def edit
