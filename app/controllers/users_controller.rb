cb9a173230210090d72de0de1e4ea281ae1ac0a5
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-11 15:10:38 +0000

    [Add] ユーザー編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      sleep(3)
+      redirect_to user_path(@user), notice: "ユーザー情報を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:name, :image_id, :email, :birthday, :sex, :intro)
+  end
+
 end
