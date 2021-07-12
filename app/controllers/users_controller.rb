3214279a119d9f349166c71cb512afda071b09bf
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-12 12:19:37 +0000

    [Add] フォロー機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-     @user = User.find_by(id: params[:id])
+      @user = User.find_by(id: params[:id])
       @reviews = @user.reviews.page(params[:page]).reverse_order
   end
 
