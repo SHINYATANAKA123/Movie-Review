ff372e1156f2d15c8f860bef409f532bde4a22f1
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-12 18:03:36 +0000

    [Add] Good，Bad機能追加

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,2 +1,19 @@
 class GoodsController < ApplicationController
+  def create
+    @review = Review.find(params[:review_id])
+    good = current_user.goods.build(review_id: params[:review_id])
+    good.save!
+    bad = Bad.find_by(user_id: current_user.id, review_id: params[:review_id])
+    if bad.present?
+      bad.destroy
+    end
+    @msg = "GOODに追加しました"
+  end
+
+  def destroy
+    @review = Review.find(params[:review_id])
+    good = Good.find_by(user_id: current_user.id, review_id: params[:review_id])
+    good.destroy!
+    @msg = "GOODから削除しました"
+  end
 end
