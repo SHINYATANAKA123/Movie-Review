fd128d7ed12e9e9c577d1bdaa93f44f6f5f48946
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-13 06:05:54 +0000

    [Add] レビュー評価機能の非同期通信化＆フラッシュ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   require 'themoviedb-api'
-  Tmdb::Api.key("05fe299182c31d92180f6f45dafaf8f1")
+  Tmdb::Api.key(ENV['API_KEY'])
   Tmdb::Api.language("ja") # こちらで映画情報の表示の際の言語設定を日本語にできます
 
   GENRES = [{id:28, name: "アクション"}, {id:12, name: "アドベンチャー"}, {id:16, name: "アニメ"}, {id:35, name: "コメディ"}, {id:80, name: "クライム"},
