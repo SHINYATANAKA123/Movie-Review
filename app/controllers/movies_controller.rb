83a45bd4199ba840d85bb7be6fff1cdfe4c1b925
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-22 07:00:29 +0000

    [fix] レイアウト修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,30 +3,10 @@ class MoviesController < ApplicationController
 
   def index
     @popular_movie = Tmdb::Movie.popular
-
-    # 外部APIを叩いて表示する項目と総件数を取得してくるケースを想定
-    # APIはpageやoffsetを指定して数十件ずつ取得するような仕様
-    @item_total = @popular_movie['item_total'].to_f
-
-    # カレントページはGETパラメーターとして受け取ってくる想定
-    @page = params['page'].to_i
-
-    # 1ページあたり30項目を表示するとして、全ページ数を取得
-    @all_page = (@item_total / 20).ceil
-
-    # 全ページ数が10以下なら1から全ページ数までを配列にする
-    # 全ページ数が10より大きいならカレントページを中心に10ページ分を配列にする
-    if @all_page > 10
-      first = [1,  @page - 4].max
-      first = [first, all_page - 9].min
-      last  = [10, @page + 5].max
-      last  = [last, @all_page].min
-    else
-      @pages = [*1..@all_page]
-    end
-
     @genres = GENRES
   end
+
+ 
 
   def show
 
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,6 @@
 module MoviesHelper
+  def image_url
+     @movie.poster_path
+  end
+
 end
