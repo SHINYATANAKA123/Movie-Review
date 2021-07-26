ed1c6b40463e5a1831c55890b0bcbb9662072727
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-26 16:27:19 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
 
  post '/homes/guest_sign_in' => 'homes#new_guest'
 
- devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+   devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: "users/omniauth_callbacks",
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
