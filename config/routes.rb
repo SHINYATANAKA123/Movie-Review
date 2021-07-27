369d9cdbea2a2bc1823da8c6222688a767b91ae8
SHINYATANAKA123 <123asde.med@gmail.com>
2021-07-27 09:37:50 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- resources :users, only: [:index, :show, :edit, :update] do
+ resources :users, only: [:index, :show, :edit, :update, :destroy] do
     resource :relationships, only: [:create, :destroy]
   end
 
